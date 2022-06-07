7e302b0eeef71e13340fe6fff3b6fda10b76bb2a
Rafal Chalupnik <rafal.chalupnik@gmail.com>
2022-06-07 19:16:05 +0200

    First basic program

--- PublisherConsole/Program.cs
+++ PublisherConsole/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using PublisherData;
+
+using var context = new PubContext();
+context.Database.EnsureCreated(); 
+
+var authors = context.Authors.ToList();
+foreach (var author in authors)
+{
+    Console.WriteLine(author.FirstName + " " + author.LastName);
+}
--- PublisherData/PubContext.cs
+++ PublisherData/PubContext.cs
@@ -8,4 +8,10 @@ public class PubContext : DbContext
     public DbSet<Author> Authors { get; set; }
     
     public DbSet<Book> Books { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Server=localhost; Database=PubDatabase; User Id=SA; Password=MyP@ssw0rd;");
+    }
 }
