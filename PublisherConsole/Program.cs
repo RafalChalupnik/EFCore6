5803a15480868bca75a8eaa56a4adc9a8092ae21
Rafal Chalupnik <rafal.chalupnik@gmail.com>
2022-06-07 19:30:15 +0200

    Reading and writing authors with books

--- PublisherConsole/Program.cs
+++ PublisherConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Microsoft.EntityFrameworkCore;
 using PublisherData;
 using PublisherDomain;
 
@@ -8,9 +9,11 @@ using (var context = new PubContext())
     context.Database.EnsureCreated(); 
 }
 
-GetAuthors();
-AddAuthor();
-GetAuthors();
+// GetAuthors();
+// AddAuthor();
+// GetAuthors();
+AddAuthorWithBooks();
+GetAuthorsWithBooks();
 
 void GetAuthors()
 {
@@ -22,6 +25,20 @@ void GetAuthors()
     }
 }
 
+void GetAuthorsWithBooks()
+{
+    using var context = new PubContext();
+    var authors = context.Authors.Include(author => author.Books).ToList();
+    foreach (var author in authors)
+    {
+        Console.WriteLine(author.FirstName + " " + author.LastName);
+        foreach (var book in author.Books)
+        {
+            Console.WriteLine("*" + book.Title);
+        }
+    }
+}
+
 void AddAuthor()
 {
     using var context = new PubContext();
@@ -30,5 +47,30 @@ void AddAuthor()
         FirstName = "Beza",
         LastName = "the Cat"
     });
+    context.SaveChanges();
+}
+
+void AddAuthorWithBooks()
+{
+    var author = new Author
+    {
+        FirstName = "Rafal",
+        LastName = "Chalupnik"
+    };
+    
+    author.Books.Add(new Book
+    {
+        Title = "Angry Hamsters",
+        PublishDate = new DateTime(year: 2022, month: 6, day: 7)
+    });
+    
+    author.Books.Add(new Book
+    {
+        Title = "Return of Angry Hamsters",
+        PublishDate = new DateTime(year: 2023, month: 7, day: 8)
+    });
+    
+    using var context = new PubContext();
+    context.Authors.Add(author);
     context.SaveChanges();
 }
