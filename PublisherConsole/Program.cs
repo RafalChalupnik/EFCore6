24eab2632746a1ec6a24bdc92f4efafd4e363933
Rafal Chalupnik <rafal.chalupnik@gmail.com>
2022-06-07 19:21:54 +0200

    Reading and writing some data

--- PublisherConsole/Program.cs
+++ PublisherConsole/Program.cs
@@ -1,12 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using PublisherData;
+using PublisherDomain;
 
-using var context = new PubContext();
-context.Database.EnsureCreated(); 
-
-var authors = context.Authors.ToList();
-foreach (var author in authors)
+using (var context = new PubContext())
 {
-    Console.WriteLine(author.FirstName + " " + author.LastName);
+    context.Database.EnsureCreated(); 
+}
+
+GetAuthors();
+AddAuthor();
+GetAuthors();
+
+void GetAuthors()
+{
+    using var context = new PubContext();
+    var authors = context.Authors.ToList();
+    foreach (var author in authors)
+    {
+        Console.WriteLine(author.FirstName + " " + author.LastName);
+    }
+}
+
+void AddAuthor()
+{
+    using var context = new PubContext();
+    context.Authors.Add(new Author
+    {
+        FirstName = "Beza",
+        LastName = "the Cat"
+    });
+    context.SaveChanges();
 }
