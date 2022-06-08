152133bbd346c574964df95c69a7aefa22653b2f
Rafal Chalupnik <rafal.chalupnik@gmail.com>
2022-06-08 19:37:37 +0200

    Seed authors migration

---
+++ PublisherData/Migrations/20220608173707_seedauthors.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PublisherData.Migrations
+{
+    public partial class seedauthors : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Authors",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[] { 1, "Rafal", "Chalupnik" });
+
+            migrationBuilder.InsertData(
+                table: "Authors",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[] { 2, "Beza", "the Cat" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
--- PublisherData/PubContext.cs
+++ PublisherData/PubContext.cs
@@ -14,4 +14,11 @@ public class PubContext : DbContext
         optionsBuilder.UseSqlServer(
             "Server=localhost; Database=PubDatabase; User Id=SA; Password=MyP@ssw0rd;");
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>().HasData(
+            new Author { Id = 1, FirstName = "Rafal", LastName = "Chalupnik" },
+            new Author { Id = 2, FirstName = "Beza", LastName = "the Cat" });
+    }
 }
